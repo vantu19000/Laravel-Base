8a50d805dc5fcb6e8dc9b9c340acf3774c223f9a
vantu.nguyen <vantu.nguyen@vietnamnet.vn>
2021-07-21 16:53:52 +0700

    init admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
 });
